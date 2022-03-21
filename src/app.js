5e17c569d841c54858bf87942012d5431c0648b8
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-03-21 14:05:24 +0100

    Set parameters for weather api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
+let apiKey = "bada8b7e78b2e8f21ed242b93f56b802";
+let unit = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=${unit}`;
 
+console.log(apiUrl);
