88449a3b94d0e79e91cc09f99b674c3240f15cef
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-03-21 14:42:23 +0100

    InnerHTML (city, temp and current state)
    Added js commands to change the properties mentioned above

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let temperatureDisplay = document.querySelector("#temperature");
   temperatureDisplay.innerHTML = Math.round(response.data.main.temp);
+  let cityDisplay = document.querySelector("#current-city");
+  cityDisplay.innerHTML = response.data.name;
+  let humidityDisplay = document.querySelector("#hum");
+  humidityDisplay.innerHTML = `Humidity: ${Math.round(
+    response.data.main.humidity
+  )}%`;
+  let windDisplay = document.querySelector("#wind");
+  windDisplay.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
+  let descriptionDisplay = document.querySelector("#weather-state");
+  descriptionDisplay.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "bada8b7e78b2e8f21ed242b93f56b802";
 let unit = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=${unit}`;
-
-console.log(apiUrl);
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 
 axios.get(apiUrl).then(displayTemperature);
